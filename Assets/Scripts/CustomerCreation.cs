2e2b496bebbb9814b1028f37f42eebb18207af39
Melody Zhu <sc21myqz@leeds.ac.uk>
2022-01-29 20:20:18 +0000

    customer creation random

--- Assets/Scripts/CustomerCreation.cs
+++ Assets/Scripts/CustomerCreation.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class CustomerCreation : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Texture[] faces;
-    public Texture[] eyes;
-    public Texture[] hairs;
-    public Texture[] mouths;
-    public Texture[] accessories;
-    public Texture[] noses;
-    void Start()
-    {
-        
+    public Sprite[] faces;
+    public Sprite[] eyes;
+    public Sprite[] hairs;
+    public Sprite[] mouths;
+    public Sprite[] accessories;
+    public Sprite[] noses;
+    public GameObject customerprefab;
+
+    void Start() {
+        CreateCharacter();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+GameObject CreateCharacter()
+{
+    GameObject newCharacter=GameObject.Instantiate(customerprefab);
+    newCharacter.transform.Find("Head").GetComponent<SpriteRenderer>().sprite = faces[Random.Range(0,faces.Length-1)];
+    newCharacter.transform.Find("Eyes").GetComponent<SpriteRenderer>().sprite = eyes[Random.Range(0,eyes.Length-1)];
+    newCharacter.transform.Find("Hair").GetComponent<SpriteRenderer>().sprite = hairs[Random.Range(0,hairs.Length-1)];
+    newCharacter.transform.Find("Mouth").GetComponent<SpriteRenderer>().sprite = mouths[Random.Range(0,mouths.Length-1)];
+    if (Random.Range(0,1) <= 0.5)
+        newCharacter.transform.Find("Accessories").GetComponent<SpriteRenderer>().sprite = accessories[Random.Range(0,accessories.Length-1)];
+    newCharacter.transform.Find("Nose").GetComponent<SpriteRenderer>().sprite = noses[Random.Range(0,noses.Length-1)];
+
+    return newCharacter;
+}
+
 }
