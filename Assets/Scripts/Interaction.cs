f20a81447447c90db61f34de28fa4d0436650c60
Dexter Lardner <ed20dl2@leeds.ac.uk>
2022-01-29 20:18:49 +0000

    InteractableObject

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interaction : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange)
+        {
+            if(Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player now in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player now out of range");
+        }
+    }
+}
