0f2ae6566ca15b29b7f11d5e1c7e58c4ba9f44c6
Raka Gunarto <rakagunarto@gmail.com>
2022-01-30 16:40:24 +0000

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Frying.cs
+++ Assets/Scripts/Frying.cs
@@ -10,18 +10,28 @@ public class Frying : Minigame
     private GameObject _window;
     private GameObject _container;
 
+    private float _posXStart;
+    private float _velocity;
+
+    private bool begin = false;
+    private bool stop = false;
+
     private int _ticks = 0;
+
+    public float acceleration;
 
     public override void Begin()
     {
         _minipan.SetActive(true);
         _window.SetActive(true);
+        begin = true;
     }
 
     public override void Cleanup()
     {
         _minipan.SetActive(false);
-        _window.SetActive(true);
+        _window.SetActive(false);
+        begin = false;
     }
 
     void Start()
@@ -31,15 +41,25 @@ public class Frying : Minigame
         _window = GameObject.FindGameObjectWithTag("FryingUIWindow");
         _container = GameObject.FindGameObjectWithTag("FryingUIContainer");
 
-        //Begin();//debug
+        _posXStart = _minipan.GetComponent<RectTransform>().position.x;
+        Begin();//debug
     }
 
     void Update()
     {
-        if (_ticks++ > (60*3))
+        if (!begin) return;
+        RectTransform panTransform = _minipan.GetComponent<RectTransform>();
+
+        if (Input.GetKeyDown(KeyCode.E))
+            stop = true;
+
+        if (!stop)
         {
-            GameObject.FindGameObjectWithTag("Timertext");
-            GameObject obj;
+            _velocity += -Mathf.Sign(panTransform.anchoredPosition.x) * acceleration * Time.deltaTime;
+            panTransform.anchoredPosition += new Vector2(_velocity * Time.deltaTime, 0);
+            return;
         }
+
+        
     }
 }
